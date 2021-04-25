fcdd39b279469e20572884d6a5c6adea3ceab56e
tuliptgr <krittinkunaree@gmail.com>
2021-04-25 15:20:34 +0700

    list

---
+++ CAMP2/25-4-SUN_pointer-struct-list-stack/InternetPackage/InternetPackage.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Package{
+	string name;
+	int sum;
+	Package(){
+		sum = 1;
+	}
+};
+bool compareByName(Package left, Package right);
+bool compareBySum(Package left, Package right);
+void print(list<Package> packages);
+
+int main(){
+	int n;
+	char c;
+	list<Package> M, F;
+	cin >> n;
+	for(int i = 0; i < n ; i++){
+		Package temp;
+		cin >> temp.name >> c;
+		bool isFound = false;
+		switch(c){
+			case 'M':
+			    for(auto it = M.begin(); it != M.end(); it++){
+                    if(it->name == temp.name){
+                        Package p;
+                        p.name = it->name;
+                        p.sum = it->sum + 1;
+                        M.insert(it,p);
+                        M.erase(it);
+                        isFound = true;
+                        break;
+                    }
+			    }
+			    if(!isFound)
+                    M.push_back(temp);
+				break;
+			case 'F':
+			    for(auto it = F.begin(); it != F.end(); it++){
+                    if(it->name == temp.name){
+                        Package p;
+                        p.name = it->name;
+                        p.sum = it->sum + 1;
+                        F.insert(it,p);
+                        F.erase(it);
+                        isFound = true;
+                        break;
+                    }
+			    }
+			    if(!isFound)
+                    F.push_back(temp);
+				break;
+		}
+	}
+	M.sort(compareByName);
+	M.sort(compareBySum);
+	F.sort(compareByName);
+	F.sort(compareBySum);
+	cout << "packages by women" << endl;
+	print(F);
+	cout << "packages by men" << endl;
+	print(M);
+	return 0;
+}
+bool compareByName(Package left, Package right){
+	if(left.name < right.name)
+		return true;
+	return false;
+}
+bool compareBySum(Package left, Package right){
+	if(left.sum > right.sum)
+		return true;
+	return false;
+}
+void print(list<Package> packages){
+	for(auto it: packages)
+		cout << it.name << ' ' << it.sum << endl;
+}
