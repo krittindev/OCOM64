9709085dfcab7984067ea594f081f9e9c9c02a5c
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2021-04-27 18:21:54 +0700

    test100

--- CAMP1/23-4-FRI_exam2/TEST0204V2/TEST0204V2.cpp
+++ CAMP1/23-4-FRI_exam2/TEST0204V2/TEST0204V2.cpp
@@ -1,126 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-long gcd(long a, long b);
-
-//void printFactor(long n, long d);
-
-/*
-bool isPrime(long n);
-void printFactorWithSet(long n);
-void setPrimes(long a, long b);
-*/
-bool isPrimeArr(long n);
-void printFactorArr(long n);
-void setPrimesArr(long a, long b);
-/*
-vector<long> primes;
-*/
-long arrPrimes[100000];
-int arrSize = 0;
+long gcd(long a, long b){
+    if(a%b == 0)
+        return b;
+    return gcd(b, a%b);
+}
+void printPrimeFac(long n){
+    if(n == 1)
+        cout << "null";
+    if(n%2 == 0){
+        while(n%2 == 0){
+            cout << 2 << ' ';
+            n /= 2;
+        }
+    }
+    for(int i = 3; i <= n; i+=2){
+        while(n%i == 0){
+            cout << i << ' ';
+            n /= i;
+        }
+    }
+    cout << endl;
+}
 int main(){
     long a, b;
     cin >> a >> b;
-    /*
-    printFactor(a, 2);
-    printFactor(b, 2);
-    if(gcd(a, b) == 1)
-        cout << "null";
-    else
-        printFactor(gcd(a, b), 2);
-    */
-    /*
-    setPrimes(a,b);
-    printFactorWithSet(a);
-    printFactorWithSet(b);
-    if(gcd(a, b) == 1)
-        cout << "null";
-    else
-        printFactorWithSet(gcd(a, b));
-    */
-    setPrimesArr(a,b);
-    printFactorArr(a);
-    printFactorArr(b);
-    if(gcd(a, b) == 1)
-        cout << "null";
-    else
-        printFactorArr(gcd(a, b));
+    printPrimeFac(a);
+    printPrimeFac(b);
+    printPrimeFac(gcd(a, b));
 	return 0;
 }
-long gcd(long a, long b){
-	if(a%b==0)
-		return b;
-	return gcd(b, a%b);
-}
-void printFactorArr(long n){
-    long sq = sqrt(n);
-    for(int i = 0; i < arrSize; i++){
-        if(n==1 || arrPrimes[i] > sq)
-            break;
-        while(n%arrPrimes[i]==0){
-            n/=arrPrimes[i];
-            cout << arrPrimes[i] << ' ';
-        }
-    }
-    if(n!=1)
-        cout << n << ' ';
-    cout << "\n";
-}
-void setPrimesArr(long a, long b){
-    if(2 <= sqrt(a>b? a: b)){
-        arrPrimes[arrSize] = 2;
-        arrSize++;
-    }
-    for(int i = 3; i <= sqrt(a>b? a: b); i+=2)
-        if(isPrimeArr(i)){
-            arrPrimes[arrSize] = i;
-            arrSize++;
-        }
-}
-bool isPrimeArr(long n){
-    for(int i = 0; i < arrSize; i++)
-        if(n%arrPrimes[i]==0)
-            return false;
-    return true;
-}
-/*
-void printFactorWithSet(long n){
-    long sq = sqrt(n);
-    for(auto it: primes){
-        if(n==1 || it > sq)
-            break;
-        while(n%it==0){
-            n/=it;
-            cout << it << ' ';
-        }
-    }
-    if(n!=1)
-        cout << n << ' ';
-    cout << "\n";
-}
-void setPrimes(long a, long b){
-    if(2 <= sqrt(a>b? a: b))
-        primes.push_back(2);
-    for(int i = 3; i <= sqrt(a>b? a: b); i+=2)
-        if(isPrime(i))
-            primes.push_back(i);
-}
-bool isPrime(long n){
-    for(auto it: primes)
-        if(n%it==0)
-            return false;
-    return true;
-}
-*/
-/*void printFactor(long n, long d){
-    //printf("\nfactorize\tn : %lld\td : %lld\n", n, d);
-	if(n == 1)
-        cout << "\n";
-    else
-            if(isPrime(d) && n%d==0){
-                cout << d << ' ';
-                printFactor(n/d, d);
-            }
-            else
-                printFactor(n, d + 1);
-}*/
--- CAMP1/23-4-FRI_exam2/TEST0204V2/usgod.cpp
+++ CAMP1/23-4-FRI_exam2/TEST0204V2/usgod.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+long long int gcd(long long int a, long long int b){
+    if(a%b == 0)
+        return b;
+    return gcd(b, a%b);
+}
+void printPrimeFac(long long int n){
+    if(n == 1)
+        cout << "null";
+    if(n%2 == 0){
+        while(n%2 == 0){
+            cout << 2 << ' ';
+            n /= 2;
+        }
+    }
+    for(int i = 3; i <= n; i+=2){
+        while(n%i == 0){
+            cout << i << ' ';
+            n /= i;
+        }
+    }
+    cout << endl;
+}
 vector<long long int> vecPrimeFac(long long int n){
     vector<long long int> result;
     if(n%2 == 0){
@@ -18,7 +40,9 @@ vector<long long int> vecPrimeFac(long long int n){
 }
 int main(){
     long long int a, b;
+    bool isHave = false;
     cin >> a >> b;
+    /*
     vector<long long int> va = vecPrimeFac(a), vb = vecPrimeFac(b);
     for(auto it: va)
         cout << it << ' ';
@@ -29,10 +53,18 @@ int main(){
     for(int i = 0; i < va.size(); i++)
         for(int j = 0; j < vb.size(); j++){
             if(va[i] == vb[j]){
+                isHave = true;
                 cout << va[i] << " ";
                 va.erase(va.begin()+i);
                 vb.erase(vb.begin()+j);
             }
         }
+    if(!isHave){
+        cout << "null";
+    }
+    */
+    printPrimeFac(a);
+    printPrimeFac(b);
+    printPrimeFac(gcd(a, b));
 	return 0;
 }
