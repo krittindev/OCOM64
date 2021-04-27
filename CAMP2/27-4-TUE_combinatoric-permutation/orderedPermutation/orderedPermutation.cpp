cd7bc2d716db28255323ce49e2537c9fa3ed2d91
tuliptgr <krittinkunaree@gmail.com>
2021-04-27 16:31:19 +0700

    orderedPer

--- CAMP2/27-4-TUE_combinatoric-permutation/orderedPermutation/orderedPermutation.cpp
+++ CAMP2/27-4-TUE_combinatoric-permutation/orderedPermutation/orderedPermutation.cpp
@@ -1,35 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int c = 0, n;
-void swap(char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-void permuta(string& str, int l, int r)
-{
-    int i;
-    if (l == r){
-        c++;
-        if(c == n)
-            cout << str << endl;
-    }
-    else
-    {
-        for(i = l; i <= r; i++)
-        {
-            swap(str[l], str[i]);
-            permuta(str, l+1, r);
-            swap(str[l], str[i]);
-        }
-    }
-}
-int main()
-{
+int main(){
+    int n;
     string str;
     cin >> str >> n;
-    permuta(str,0,str.size()-1);
+    n--;
+    sort(str.begin(), str.end());
+    while (next_permutation(str.begin(), str.end()) && --n);
+    cout << str;
     return 0;
 }
