7ea6ada08fcbf9ce8a4426ccb205dd8110192e1f
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2021-05-03 17:14:20 +0700

    egypt

--- CAMP2/3-5-MON_greedy/egyptFraction/egyptFraction.cpp
+++ CAMP2/3-5-MON_greedy/egyptFraction/egyptFraction.cpp
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float x, y;
+    int x, y, nx, ny;
     cin >> x >> y;
     while(1){
-        cout << x << ' ' << y << endl;
-        if((((int)(-y)%(int)x)+(int)x)%(int)x!=1){
-            cout << ceil(y/x);
+        cout << ceil((float)(y)/(float)(x)) << endl;
+        nx = ((-y) % x + x ) % x;
+        ny = y * ceil((float)(y)/(float)(x));
+        x = nx;
+        y = ny;
+        if(x == 0)
             break;
-        }
-        cout << ceil(y/x) << endl;
-        x = (((int)(-y)%(int)x)+(int)x)%(int)x;
-        y = y * ceil(y/x);
     }
     return 0;
 }
