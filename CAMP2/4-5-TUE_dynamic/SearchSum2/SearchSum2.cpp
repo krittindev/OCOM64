9ca456cf68f0ee6c8059cd55dbc011033999d6a8
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2021-05-05 01:32:12 +0700

    4-5

--- CAMP2/4-5-TUE_dynamic/SearchSum2/SearchSum2.cpp
+++ CAMP2/4-5-TUE_dynamic/SearchSum2/SearchSum2.cpp
@@ -1,1 +1,36 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int searchSum(int*, int, int);
+int main(){
+    int n, k, temp;
+    cin >> n >> k;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    while(k--){
+        cin >> temp;
+        cout << searchSum(arr, n, temp) << endl;
+    }
+    return 0;
+}
+int searchSum(int arr[], int n, int t){
+    int p = arr[0], s = 0, counter = 0;
+    if(arr[0] > t)
+        counter = 0;
+    else
+        counter = 1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > t)
+            continue;
+        if(p > t ||  p + arr[i] > t ){
+            counter = 1;
+            p = arr[i];
+        }
+        else{
+            counter++;
+            p += arr[i];
+        }
+        s = max(s, counter);
+    }
+    return s;
+}
--- CAMP2/4-5-TUE_dynamic/longest2/longest2.cpp
+++ CAMP2/4-5-TUE_dynamic/longest2/longest2.cpp
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int LCM(int*, int*, int);
+int mem[1000];
+int LIS(int*, int*, int);
 int main(){
     int n, arr[1000], L[1000] = {0}, maxL = 0;
     cin >> n;
     for(int i = 0; i < n; i++)
         cin >> arr[i];
     for(int i = 0; i < n; i++)
-        if(LCM(arr, L, i) > maxL)
-            maxL = LCM(arr, L, i);
-    /*for(int i = 0; i < n; i++)
-        cout << i << ' ' << arr[i] << ' ' << maxL + 1 << ' ' << L[i] << endl;*/
+        if(LIS(arr, L, i) > maxL)
+            maxL = LIS(arr, L, i);
+    for(int i = 0; i < n; i++)
+        cout << i << ' ' << arr[i] << ' ' << maxL + 1 << ' ' << L[i] << endl;
     cout << maxL << endl;
     maxL = 0;
-    for(int i = 0; i < n; i++)
-        if(maxL < L[i]){
-            cout << arr[i] << " ";
-            maxL = L[i];
-        }
+    for(int i = 0; i < maxL; i++)
+        cout << mem[i] << " ";
     return 0;
 }
-int LCM(int arr[], int L[], int n){
+int LIS(int arr[], int L[], int n){
     int maxL = 0;
     for(int i = 0; i < n; i++)
         if(arr[i] < arr[n]){
             if(L[i] == 0)
-                L[i] = LCM(arr, L, i);
+                L[i] = LIS(arr, L, i);
             if(maxL < L[i])
                 maxL = L[i];
         }
