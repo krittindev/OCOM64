0e4ef4d11ed8639f51f8556edffa866966ae6e5c
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2021-05-04 14:49:02 +0700

    long1

--- CAMP2/4-5-TUE_dynamic/SearchSum1/SearchSum1.cpp
+++ CAMP2/4-5-TUE_dynamic/SearchSum1/SearchSum1.cpp
@@ -1,12 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n, k;
+    int n, k, temp;
     cin >> n >> k;
     int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 1; i < n; i++)
+        arr[i] += arr[i - 1];
     while(k--){
         cin >> temp;
-        cout << searchsum(temp);
+        int i;
+        for(i = n - 1; i > 0; i--)
+            if(temp >= arr[i])
+                break;
+        if(i == 0 && temp < arr[i])
+            cout << 0 << endl;
+        else
+            cout << i + 1 << endl;
     }
     return 0;
 }
---
+++ CAMP2/4-5-TUE_dynamic/longest1/longest1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int LCM(int*, int*, int);
+int main(){
+    int n, arr[1000], L[1000] = {0}, maxL = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++)
+        if(LCM(arr, L, i) > maxL
+            maxL = LCM(arr, L, i);
+            cout << i << ' ' << arr[n] << ' ' << maxL << endl;
+        }
+    cout << maxL << endl;
+    return 0;
+}
+int LCM(int arr[], int L[], int n){
+    int maxL = 0;
+    for(int i = 0; i < n; i++)
+        if(arr[i] < arr[n]){
+            if(L[i] == 0)
+                L[i] = LCM(arr, L, i);
+            if(maxL < L[i])
+                maxL = L[i];
+        }
+    L[n] = maxL + 1;
+    return L[n];
+}
